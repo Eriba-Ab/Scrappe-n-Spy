5bf67aa76fb0aa6890ad07e7184bc2e00981efaa
Abraham Eriba <eribainc@gmail.com>
2023-06-05 14:25:15 +0100

    Refactored it. Changed a few data structures

--- Scrapper.py
+++ Scrapper.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 import time
-import os
 
 # Constants
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
